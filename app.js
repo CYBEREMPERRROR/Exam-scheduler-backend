93e02bd88fe202e2aa8e9c0ac0a074b764c9e551
Science Scheduler Team <scheduler@sciencefaculty.edu>
2026-02-13 01:03:36 +0100

    Move exams.js into routes folder

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // app.js
 const express = require('express');
 require('dotenv').config();
+
 const examsRouter = require('./routes/exams');
 
 const app = express();
 app.use(express.json());
 
-// Routes
-app.use('/exams', examsRouter);
+app.use('/exams', examsRouter);  // <-- NO parentheses here
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
